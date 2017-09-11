89ab6e1bea3188b03fb59aecc0927778e7c04923
willybeans <willwedmedyk@gmail.com>
2017-09-11 13:25:27 -0400

    search and replace algorithm for FCC

---
+++ search_and_replace.js
@@ -0,0 +1,22 @@
+function myReplace(str, before, after) {
+  var array = str.split(" ");
+  var fixCase;
+  
+    before = before.split("");
+    after = after.split("");
+  //compare and replace with uppercase
+      if(before[0] === before[0].toUpperCase()){
+        after.splice(0,1,after[0].toUpperCase());
+      }
+    before = before.join("");
+    after = after.join("");
+  //find word to replace
+    for(var i=0;i<array.length;i++){
+      if(array[i] === before){
+        array.splice(i, 1, after);
+      }
+    }
+   return array.join(" ");
+}
+
+myReplace("He is Sleeping on the couch", "Sleeping", "sitting");
