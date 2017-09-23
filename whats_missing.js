30ef81215d9ab1480db8041c3994b044209efed5
willybeans <willwedmedyk@gmail.com>
2017-09-23 14:26:31 -0400

    added console logs

--- whats_missing.js
+++ whats_missing.js
@@ -3,15 +3,27 @@ function fearNotLetter(str) {
   //97 = a || 122 = z
   //65 = A || 90 = Z
   var array = str.split('');
-  var id;
+  var end;
+  var start = array[0];
+  var end;
+  var length = array.length;
+
     for (let i = 0; i < array.length; i++) {
-         id = array[i]
-      // i === array.length then this is the last number
-      // i === 0 then this is the first number
-      //simply create a new array that has numbers spanning from start to finish
-      //wait. but you need to return the missing character...
-      //ok so compare the existing array with the new array and if {[x]doesnt //match} the we return that non matched number... :-) OK BYE
+         if (i === 0){
+           start = array[i].charCodeAt();
+         } else if (i = length) {
+           end = array[i].charCodeAt();
          }
-  console.log(id);
+
+    }
+  console.log("length: " + length);
+  console.log("start: " + start);
+  console.log("end" + end);
 }
 fearNotLetter("abce");
+
+// i === array.length then this is the last number
+// i === 0 then this is the first number
+//simply create a new array that has numbers spanning from start to finish
+//wait. but you need to return the missing character...
+//ok so compare the existing array with the new array and if {[x]doesnt //match} the we return that non matched number... :-) OK BYE
