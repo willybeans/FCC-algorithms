907fb6d42b23b90417131ae9560a9687a55cc1ab
willybeans <willwedmedyk@gmail.com>
2017-12-04 09:43:08 -0500

    this is the solution

--- sum_of_odd_fibonacci.js
+++ sum_of_odd_fibonacci.js
@@ -1,86 +1,19 @@
-// function sumFibs(num) {
-//   var a = 1;
-//   var b = 1;
-//   var total = 0;
-//   var oddTotal = 2;
-//   var finalSum;
-//
-//   while (oddTotal < num){
-//    if (total % 2 != 0){
-//         oddTotal = oddTotal + total;
-//         finalSum = oddTotal;
-//         if(oddTotal < num) { //this is to fix the issue of adding to total TOO SOON
-//           finalSum = oddTotal;
-//           console.log(finalSum+"_");
-//         }
-//       }
-//       total = a + b;
-// you need to add a condition in here to check the value of the two integers before being added together.
-       // if (a < total && b < total) {
-       //     you know what i mean?
-       //    }
-//       console.log("a: " + a);
-//       console.log("b: " + b);
-//       b = a;
-//       a = total;
-//
-//     }
-//     console.log(finalSum);
-// }
-//
-// sumFibs(1000);
-//this doesnt work ....
-// function sumFibs(num) {
-//   var a = 1;
-//   var b = 1;
-//   var total = 0;
-//   var oddTotal = 2;
-//   var finalSum;
-//
-// do {
-//   if (total % 2 != 0){
-//        oddTotal = oddTotal + total;
-//        if(oddTotal < num) {
-//          finalSum = oddTotal;
-//          console.log(finalSum+"_");
-//        }
-//      }
-//      total = a + b;
-//      console.log("a: " + a);
-//      console.log("b: " + b);
-//      b = a;
-//      a = total;
-//
-//    }
-// while (oddTotal < num);
-//  console.log(finalSum);
-// }
-//
-// sumFibs(1000);
-
 function sumFibs(num) {
   var a = 1;
   var b = 1;
   var total = 1;
   var oddTotal = 2;
-  var final = 0;
 
   while (total < num){
-
       b = a;
       a = total;
       total = a + b;
-
-       if (total % 2 != 0){
-           if(oddTotal < num) {
+       if (total % 2 != 0 && total <= num){
              oddTotal = oddTotal + total;
-             final = oddTotal;
-           }
        }
-
-
     }
-    return final;
+    console.log(oddTotal);
+    return oddTotal;
 }
 
 sumFibs(75025);
