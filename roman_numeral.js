f83f321038a5d0531523e688c6e7826f6e1420f9
willybeans <willwedmedyk@gmail.com>
2018-01-06 11:55:47 -0500

    removed refactored functions. They were too small to justify

--- roman_numeral.js
+++ roman_numeral.js
@@ -2,19 +2,10 @@ const roman = {
   1: "I", 4: "IV", 5: "V", 6: "VI",9: "IX",10: "X",40: "XL",50: "L",60: "LX",90: "XC",
   100: "C", 400: "CD", 500: "D", 600: "DC", 900: "CM", 1000: "M"
 };
-
-function firstDigit(x) { //supplies you with last digit, need to then divide by 10.
-   return x % 10;
-}
-
-function reduceNum(x) { //remove lowest digit in number
-  return Math.floor(x / 10);
-}
-
 function convertToRoman(num){
   var counter = 1, array = [], first, final;
   while (num > 0) {
-    first = firstDigit(num) * counter;
+    first = (num % 10) * counter;//gives you smallest digit
     while (first > 0) {
       if (roman[first]) {
         array.splice(0,0,roman[first]);
@@ -25,7 +16,7 @@ function convertToRoman(num){
       }
     }
     counter *= 10;
-    num = reduceNum(num);
+    num = Math.floor(num / 10); //remove lowest digit
   }
   console.log(array.join(''));
   // return array.join('');
