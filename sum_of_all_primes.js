924b0521606aa8852646d1f7e71ed0be89618ce1
willybeans <willwedmedyk@gmail.com>
2018-01-11 11:05:54 -0500

    finished

--- sum_of_all_primes.js
+++ sum_of_all_primes.js
@@ -1,28 +1,3 @@
-// function sumPrimes(num) {
-//
-//   var final = 0;
-//
-//   for (var i = 0; i < num; i++){
-//     if(i > 1 && i <= num){
-//      // console.log("first: "+i);
-//       if (i === 2 || i === 3){
-//         final = final+i;
-//       } else if ((i%2 === 0 || i%3 === 0)){
-//        // console.log("not prime #: "+i);
-//       } else {
-//         console.log("prime: "+i);
-//         final = final+i;
-//      //   console.log("calc: "+final)
-//       }
-//
-//     }
-//   }
-//
-//   console.log(final);
-// }
-//
-// sumPrimes(977);
-
 function sumPrimes(num) {
   var total = 0, sieve = [], primes = [];
   for (var i = 2; i <= num; i++){
@@ -33,10 +8,9 @@ function sumPrimes(num) {
       }
     }
   }
-for (i = 0; i < primes.length; i++){
-  total = total + primes[i];
-}
+  for (i = 0; i < primes.length; i++){
+    total = total + primes[i];
+  }
 console.log(total);
 }
-
 sumPrimes(10);
