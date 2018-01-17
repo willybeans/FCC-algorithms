d00ba0be1d1404407f4f2f6b6ebedcc1dc0e0574
willybeans <willwedmedyk@gmail.com>
2018-01-16 23:49:45 -0500

    first commit. create a closure that adds params

---
+++ arguments_optional.js
@@ -0,0 +1,11 @@
+/* create a closure that adds the two received arguments
+ex: (2,3) should evaluate to 5
+*/
+function addTogether() {
+  var x = [...args];
+  var sumTwo = function(y) {
+
+  }
+}
+
+addTogether(2,3);
