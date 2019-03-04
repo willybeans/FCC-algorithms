e42187fd54e44a1580ab35507aa1282f7db725e3
willybeans <willwedmedyk@gmail.com>
2019-03-04 14:28:03 -0500

    algs

--- cashregister.js
+++ cashregister.js
@@ -21,52 +21,77 @@ function checkCashRegister(price, cash, cid) {
       change: []
     };
 
-    let change = cash - price;
+    let ammountDue = cash - price;
     let arrayIndex = cid.length - 1;
 
-    if(change > 0) {
-      returnMessage = cashierGivesChange(cid, change, returnValues, moneyValues, returnMessage);
+    if(ammountDue > 0) {
+      returnMessage = cashierGivesammountDue(cid, ammountDue, returnValues, moneyValues, returnMessage);
+      console.log(returnMessage);
       return returnMessage;
-    } else if(change === 0) {
-      returnMessage.status = returnValues.CLOSED;
+    } else if(ammountDue === 0) {
+      returnMessage.status = returnValues.OPEN;
     return returnMessage;
   }
 
 }
 
-function cashierGivesChange(cid, change, returnValues, moneyValues, returnMessage){
-
+function cashierGivesammountDue(cid, ammountDue, returnValues, moneyValues, returnMessage){
   let totalCashInDrawer = 0;
   cid.forEach( x => {
     totalCashInDrawer = totalCashInDrawer + x[1];
   });
   let roundedTotal = totalCashInDrawer.toFixed(2);
+  let arrayIndex = cid.length - 1;
 
-  if(roundedTotal < change) {
+  if(roundedTotal < ammountDue) { //return if there isnt enough money
     returnMessage.status = returnValues.INSUFICIENT_FUNDS;
     return returnMessage;
-  } else if (roundedTotal === change) {
-    reutnrMessage.status = returnValues.CLOSED;
+  } else if (roundedTotal === ammountDue) { // return if change is exact to drawer
+    returnMessage.status = returnValues.CLOSED;
     returnMessage.change = cid;
-  } else {
-    while ((change > 0) && (arrayIndex >= 0)) {
+    return returnMessage;
+  } else { //begin converting ammountDue to change for customer
+    while ((ammountDue > 0) && (arrayIndex >= 0)) {
       let currentCurrency = moneyValues[cid[arrayIndex][0]];
-      if(currentCurrency <= change) {
+      if(currentCurrency <= ammountDue) {
         cid[arrayIndex][1] -= currentCurrency;
-        change -= currentCurrency;
+        ammountDue -= currentCurrency;
+        if (returnMessage.change.length === 0){
+          returnMessage.change.push([cid[arrayIndex][0], currentCurrency]);
+        } else {
+          console.log('length ' + returnMessage.change.length);
+          for (let i = 0; i < returnMessage.change.length; i++) {
+            console.log(returnMessage.change[i]);
+            console.log('current ammount ' + returnMessage.change[i][1]);
+            console.log('money values' + cid[arrayIndex][0])
+            if (returnMessage.change[i][0] === cid[arrayIndex][0]){
+              returnMessage.change[i][1] += currentCurrency;
+            } else if (i === returnMessage.change.length - 1
+              && returnMessage.change[i][0] != cid[arrayIndex][0]) {
+                returnMessage.change.push([cid[arrayIndex][0], currentCurrency]);
+              }
+          }
+        }
+        //returnMessage.change.push([cid[arrayIndex][0], currentCurrency]);
+          console.log(
+            `
+          ammountDue: ${ammountDue}
+          currentCurrency: ${currentCurrency}
+          returnMessage.change: ${returnMessage.change}
+          returnMessage.change[0]: ${returnMessage.change[0][0]}
+          returnMessage.change[1]: ${returnMessage.change[1]}
+          returnMessage.change.length: ${returnMessage.change.length}
+            `
+          );
       } else {
-          console.log('else');
-          arrayIndex--;
+        arrayIndex--;
       }
     }
-
+    //add a thing to say closed if change in drawer is empty
+    return returnMessage;
   }
-
-
-
-
 
 }
 
-checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1],
+checkCashRegister(14, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1],
 ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
--- phone_number.js
+++ phone_number.js
@@ -1,4 +1,4 @@
-/*
+/* THIS IS ALL WRONG LOLOLOL
 Return true if the passed string is a valid US phone number.
 
 The user may fill out the form field any way they choose as long as it is a valid US number.
@@ -14,7 +14,7 @@ For this challenge you will be presented with a string such as 800-692-7753 or 8
 Your job is to validate or reject the US phone number based on any combination of the formats provided above.
 The area code is required. If the country code is provided, you must confirm that the country code is 1.
 Return true if the string is a valid US phone number; otherwise return false.
- */
+ 
 
  //conditional statement if str is > 10 the first num needs to equal 1
 function telephoneCheck(str) {
@@ -51,3 +51,5 @@ function falsy() {
 
 
 telephoneCheck("5555555555");
+
+*/
